33d5a972b3b21ee3b5b15dad6cf8d6f3155f1ee1
andrea scacchi <andrea.scacchi@live.com>
2022-05-05 16:58:07 +0200

    ConsoleLog FizzBuzz completed, now onto styling DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // SCRIPT FOR FIZZBUZZDOM
+
+console.log("helloworld!");
+
+for ( let i = 1; i < 101 ; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+    } else if ( i % 3 == 0 ) {
+        console.log("Fizz");
+    } else if (i % 5 == 0 ){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }   
+
+}
+
