83c2c084ab443ac98399087753c333d23c2375ad
andrea scacchi <andrea.scacchi@live.com>
2022-05-05 18:22:45 +0200

    Final commit, complete exercise. cleaned code.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // SCRIPT FOR FIZZBUZZDOM
 
+//debug code
+/*
 console.log("helloworld!");
-
 for ( let i = 1; i < 101 ; i++){
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz")
@@ -11,10 +12,9 @@ for ( let i = 1; i < 101 ; i++){
         console.log("Buzz");
     } else {
         console.log(i);
-    }   
-
+   }   
 }
-
+*/
 
 
 for ( let i = 1; i < 101 ; i++) {
@@ -22,14 +22,23 @@ for ( let i = 1; i < 101 ; i++) {
     const grid = document.querySelector("div.grid");
     //create a div inside
     const div = document.createElement(`div`);
-
-    // const box = document.querySelector("div.box");
-    // const p = document.createElement('p');
-
+    //adding .box to div
     div.classList.add('box');
+    //appending div to div.grid
     grid.append(div);
-    // 
-    div.append(i);
-    // 
+
+    //create a if statement
+    if (i % 3 == 0 && i % 5 == 0) {
+        div.classList.add('fizzbuzz');
+        div.append('FizzBuzz');
+    } else if ( i % 3 == 0 ) {
+        div.classList.add('fizz');
+        div.append('Fizz');
+    } else if ( i % 5 == 0 ) {
+        div.classList.add('buzz');
+        div.append('Buzz');
+    } else {
+        div.append(i);
+    }
 
 }
