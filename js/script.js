55c1cceef272ff62b79a42a8fbffdb0cbcb6e2ac
andrea scacchi <andrea.scacchi@live.com>
2022-05-05 17:41:45 +0200

    mid-work commit, added flexbox div element generated via js cycle now onto special if cases

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ for ( let i = 1; i < 101 ; i++){
 
 }
 
+
+
+for ( let i = 1; i < 101 ; i++) {
+    //select div.grid from dom
+    const grid = document.querySelector("div.grid");
+    //create a div inside
+    const div = document.createElement(`div`);
+
+    // const box = document.querySelector("div.box");
+    // const p = document.createElement('p');
+
+    div.classList.add('box');
+    grid.append(div);
+    // 
+    div.append(i);
+    // 
+
+}
